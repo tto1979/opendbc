dad27f4b271ffe94731aa933d4b04d8016fbfe4e
CT921 <tim.chen242@gmail.com>
2025-04-02 14:31:18 +0800

    Update carcontroller.py

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -67,8 +67,8 @@ def poll_blindspot_status(lr):
 
 def get_long_tune(CP, params):
   if Params().get_bool("ToyotaTune"):
-    kiBP = [0., 25]
-    kiV = [0.8, 1.2]
+    kiBP = [0., 32]
+    kiV = [0.4, .2]
     if CP.carFingerprint in TSS2_CAR:
       kiBP = [2., 5.]
       kiV = [0.5, 0.25]
