3abb939c5f2a256b704b21955226457b9dff5387
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-10-15 21:07:09 +0800

    Update can.h

--- opendbc/safety/can.h
+++ opendbc/safety/can.h
@@ -4,7 +4,8 @@ static const unsigned char dlc_to_len[] = {0U, 1U, 2U, 3U, 4U, 5U, 6U, 7U, 8U, 1
 
 #define CANPACKET_HEAD_SIZE 6U  // non-data portion of CANPacket_t
 
-#ifdef CANFD
+#if !defined(STM32F4)
+  #define CANFD
   #define CANPACKET_DATA_SIZE_MAX 64U
 #else
   #define CANPACKET_DATA_SIZE_MAX 8U
@@ -24,4 +25,6 @@ typedef struct {
   unsigned char data[CANPACKET_DATA_SIZE_MAX];
 } __attribute__((packed, aligned(4))) CANPacket_t;
 
+#define GET_BUS(msg) ((msg)->bus)
 #define GET_LEN(msg) (dlc_to_len[(msg)->data_len_code])
+#define GET_ADDR(msg) ((msg)->addr)
