536dd88ab1988b30f24edb4046c4efa00da22dc7
【TIM】 <90225659+CT921@users.noreply.github.com>
2025-12-09 14:02:20 +0800

    Update toyota.h

--- opendbc/safety/modes/toyota.h
+++ opendbc/safety/modes/toyota.h
@@ -173,7 +173,6 @@ static void toyota_rx_hook(const CANPacket_t *msg) {
     if (msg->addr == 0x1D3U) {
       // ACC main switch on is a prerequisite to enter controls, exit controls immediately on main switch off
       // Signal: PCM_CRUISE_2/MAIN_ON at 15th bit
-      bool acc_on_dbc = (GET_BIT(msg, 15U) != 0U);
       acc_main_on = GET_BIT(msg, 15U) != 0U;
     }
 
